182dd3033ecedd97d106251aaab1c744c5c4dbbc
Dima Pivovarov <pivovarovdi@gmail.com>
2019-07-09 03:05:32 +0300

    Homework 3

---
+++ app/src/main/java/com/loftschool/pivovarov/loftmoney1/ItemsAdapter.java
@@ -0,0 +1,60 @@
+package com.loftschool.pivovarov.loftmoney1;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
+
+    private List<Item> mItemList = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ItemsAdapter.ItemViewHolder onCreateViewHolder(
+            @NonNull final ViewGroup viewGroup, final int i)
+    {
+        View itemView = View.inflate(viewGroup.getContext(), R.layout.item_view, null);
+        return new ItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ItemsAdapter.ItemViewHolder viewHolder, final int i) {
+        final Item item = mItemList.get(i);
+        viewHolder.bindItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemList.size();
+    }
+
+    public void addItem(final Item item) {
+        mItemList.add(item);
+        notifyItemInserted(mItemList.size());
+    }
+
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mNameView;
+        private TextView mPriceView;
+
+        public ItemViewHolder(@NonNull final View itemView) {
+            super(itemView);
+
+            mNameView = itemView.findViewById(R.id.item_name);
+            mPriceView = itemView.findViewById(R.id.item_price);
+        }
+
+        public void bindItem(final Item item) {
+            mNameView.setText(mNameView.getText().toString() + " " + item.getName());
+            mPriceView.setText(
+                    mPriceView.getContext().getResources().getString(R.string.price_template, String.valueOf(item.getPrice())));
+        }
+    }
+}
